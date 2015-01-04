aeddc835cbc001ea4307b6aefef998591d610faa
witoza <witold.zegarowski@gmail.com>
2015-01-04 18:37:33 +0100

    possibility to invoke several pages at the same time

--- js/kickass.js
+++ js/kickass.js
@@ -1,31 +1,17 @@
 "use strict";
 
-function augmentKickass() {
-	var opts = myOPT.opts;
 
-	if (!opts.General.Integrate_with_Kickass) {
-		console.log("Kickass is selected not to be integrated, skipping");
-		return;
-	}
+function isKickassMovies() {
+	return window.location.pathname.indexOf("/movies/1/") >= 0;
+}
 
-	createOptionsBreadcrumbsNode().insertBefore($(".mainpart"));
+function impl(main_part, opts) {
 
+	main_part.find(".sidebarCell").remove();
+	main_part.find(".advertising").remove();
+	 
 
-	if (opts.General.Remove_adds) {
-		console.log("[MAIN] Removing adds");
-		$('#sidebar').remove();
-		$('iframe').remove();
-		$('.advertising').remove();
-		$('.feedbackButton').remove();
-
-	}
-	if (!opts.General.Enable_augmenting){
-		console.log("[MAIN] Augmenting has been disabled");
-		return;
-	}
-
-
-	var main_node = $('.mainpart').find("table.data:first").find("tbody");
+	var main_node = main_part.find("table.data:first").find("tbody");
 	var resultSet = main_node.find(".firstr");
 
 	var x = resultSet.children(1).first();
@@ -45,7 +31,9 @@ function augmentKickass() {
 	main_node.children().each(function(index) {
 		if (index < 1) return;
 
-		var titleNode = $(this).find(".markeredBlock .cellMainLink");
+		var curr = $(this);
+
+		var titleNode = curr.find(".markeredBlock .cellMainLink");
 
 		var originalTitle = titleNode.text();
 		console.log("-------");
@@ -53,13 +41,13 @@ function augmentKickass() {
 
 		if (opts.Integration.Hide_Hindi_versions && isMovieAHindi(originalTitle)){
 			console.log("movie '" + originalTitle + "' is HINDI - skipping display");
-			$(this).hide(500);
+			curr.remove();
 			return;
 		}
 		
 		if (opts.Integration.Hide_CAM_versions && isMovieACam(originalTitle)){
 			console.log("movie '" + originalTitle + "' is CAM - skipping display");
-			$(this).hide(500);
+			curr.remove();
 			return;
 		}
 		var cleanedTitle = getCleanTitleGeneric(originalTitle);
@@ -69,24 +57,24 @@ function augmentKickass() {
 		}
 		if (isMovieAlreadyBlacklisted(cleanedTitle)) {
 			console.log("movie '" + cleanedTitle.title + "' is blacklisted");
-			$(this).hide(500);
+			curr.remove();
 			return;
 		}
 
 
 		if (opts.FilmWeb.Integrate_with_FilmWeb) {
 			var filmwebNode = $("<td>" + getAjaxIcon() + "</td>");
-			$(this).append(filmwebNode);
+			curr.append(filmwebNode);
 			addFilmwebCell(filmwebNode, cleanedTitle);
 		}
 		if (opts.IMDB.Integrate_with_IMDB) {
 			var imdbNode = $("<td>" + getAjaxIcon() + "</td>");
-			$(this).append(imdbNode);
+			curr.append(imdbNode);
 			addIMDBCell(imdbNode, cleanedTitle);
 		}
 		if (opts.Links.Add_links) {
 			var linksNode = $("<td></td>");
-			$(this).append(linksNode);
+			curr.append(linksNode);
 			addLinksCell(linksNode, originalTitle, cleanedTitle);
 		}
 
@@ -94,6 +82,60 @@ function augmentKickass() {
 	});
 
 	console.log("[MAIN] End of scanning");
+
+}
+
+
+function add_more_pages(page_nbr, max_pages, main_part, opts) {
+	if (page_nbr >  max_pages) return;
+
+	callAjax("", {
+		beforeSend : function () {
+			console.log("getting another page");
+		},
+		url : "https://kickass.so/movies/" + page_nbr,
+		success : function(data) {
+				var main_part2 = $(data).find(".mainpart");
+				impl(main_part2, opts);
+				//main_part.find(".pages").remove();
+				main_part.parent().append(main_part2);
+
+				add_more_pages(page_nbr + 1, max_pages, main_part, opts);
+		}
+	});
+
+}
+
+function augmentKickass() {
+	var opts = myOPT.opts;
+
+	if (!opts.General.Integrate_with_Kickass) {
+		console.log("Kickass is selected not to be integrated, skipping");
+		return;
+	}
+
+	if (opts.General.Remove_adds) {
+		console.log("[MAIN] Removing adds");
+		$('#sidebar').remove();
+		$('iframe').remove();
+		$('.feedbackButton').remove();
+
+	}
+	if (!opts.General.Enable_augmenting){
+		console.log("[MAIN] Augmenting has been disabled");
+		return;
+	}
+
+
+	var main_part = $(".mainpart");
+
+	createOptionsBreadcrumbsNode().insertBefore(main_part);
+
+	impl(main_part, opts);
+
+	if (isKickassMovies()) {
+		add_more_pages(2, 4, main_part, opts);
+	}
 
 
 }
